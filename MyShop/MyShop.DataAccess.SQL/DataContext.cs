2816f4f20160d68c8563947c2e156398878b7e92
Oumeir-Rengony-Hangar <oumeir.rengony@hangarww.com>
2020-09-07 11:33:42 +0400

    Added User Registration

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -19,6 +19,6 @@ namespace MyShop.DataAccess.SQL
         public DbSet<ProductCategory> ProductCategories{ get; set; }
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<BasketItem> BasketItems { get; set; }
-
+        public DbSet<Customer> customers { get; set; }
     }
 }
---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202009070729268_AddCustomers1.cs
@@ -0,0 +1,20 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCustomers1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "FirstName", c => c.String());
+            DropColumn("dbo.Customers", "FirtsName");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Customers", "FirtsName", c => c.String());
+            DropColumn("dbo.Customers", "FirstName");
+        }
+    }
+}
